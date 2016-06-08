3dfc4ca0816c38aa58f44a16e0ffb11a8164f3e6
Paul Bartlett <bartlett_paul@hotmail.com>
2016-06-08 13:57:31 -0400

    added more to be ignored, added functionality for 3.0

--- AlbumArtRemoval.ps1
+++ AlbumArtRemoval.ps1
@@ -1,16 +1,22 @@
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
-Function Get-Folder($initialDirectory)
 
+Function Get-Folder($initialDirectory)
 {
     $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
     $foldername.rootfolder = "MyComputer"
 
     if($foldername.ShowDialog() -eq "OK")
     {
-        $folder += $foldername.SelectedPath
+        return $foldername.SelectedPath
     }
-    return $folder
+    else
+	{
+		Exit 0
+	}
 }
-
 $Path = Get-Folder
-Remove-Item -path $Path -recurse -include *.jpg, *.png -exclude folder.*, cover.*
+
+<# Statement not supported in 3.0 due to bug with removing
+Remove-Item -path $Path -recurse -include *.jpg, *.png -exclude folder.*, cover.*, *$(($Path -split '\\')[-1])*, *$(($Path -split '\\')[-2])* 
+#>
+Get-ChildItem -path $Path -include *.jpg, *.png -exclude folder.*, cover.*, *$(($Path -split '\\')[-1])*, *$(($Path -split '\\')[-2])* -recurse | Remove-Item
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Album Art Removal
 =================
-Removes unnecessary album art files that occur from ripping cd's with various programs. Runs recursively on all subfolders of the chosen folder.
+Removes unnecessary album art files that occur from ripping cd's with various programs. Runs recursively on all subfolders of the chosen folder. If the folder structure is setup as Artist -> Album -> Songs, then any image name with the artist or album in it will be kept. Also ignores cover.* and folder.*. Works with PowerShell 3.0+.
